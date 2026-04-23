db560e5a75d3a0f7178cdc3c30c0270e7e9956af
Brian L Wong <blw@blw.net>
2026-04-23 18:43:24 -0400

    Revert "TopoShape + PartDesign::Boolean: fuzzy matching fix and bakeInTransform"
    This reverts commit c2843fc2ddbb00e7fd527d74a1e0832b1a7106f0.

--- src/Mod/PartDesign/App/FeatureBoolean.cpp
+++ src/Mod/PartDesign/App/FeatureBoolean.cpp
@@ -155,8 +155,6 @@ App::DocumentObjectExecReturn* Boolean::execute()
         }
     }
 
-    result.bakeInTransform();
-
     result = refineShapeIfActive(result);
 
     if (!isSingleSolidRuleSatisfied(result.getShape())) {
@@ -221,10 +219,6 @@ void Boolean::onChanged(const App::Property* prop)
 
     if (strcmp(prop->getName(), "Group") == 0) {
         touch();
-    }
-
-    if (strcmp(prop->getName(), "Shape") == 0) {
-        updatePreviewShape();
     }
 
     Feature::onChanged(prop);
